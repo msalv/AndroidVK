396300822c484890b4d1c15e81c1075f8c8f0708
Kirill Bykov <bikovgta13@gmail.com>
2015-12-13 14:02:34 +0300

    Show LoginErrorFragment if auth failed

--- app/src/main/java/org/kirillius/friendslist/MainActivity.java
+++ app/src/main/java/org/kirillius/friendslist/MainActivity.java
@@ -1,43 +1,41 @@
 package org.kirillius.friendslist;
 
+import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.widget.Toast;
 
 import com.vk.sdk.VKAccessToken;
 import com.vk.sdk.VKCallback;
 import com.vk.sdk.VKSdk;
-import com.vk.sdk.api.VKApi;
 import com.vk.sdk.api.VKError;
-import com.vk.sdk.api.VKParameters;
-import com.vk.sdk.api.VKRequest;
-import com.vk.sdk.api.VKResponse;
-import com.vk.sdk.api.model.VKApiUserFull;
-import com.vk.sdk.api.model.VKList;
 
 import org.kirillius.friendslist.core.AppLoader;
+import org.kirillius.friendslist.fragments.LoginErrorFragment;
 import org.kirillius.friendslist.fragments.FriendsFragment;
-import org.kirillius.friendslist.ui.FriendsAdapter;
 
-public class MainActivity extends AppCompatActivity {
-
-    private Toolbar mToolbar;
+public class MainActivity extends AppCompatActivity implements LoginErrorFragment.OnLoginAttemptListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mToolbar = (Toolbar) findViewById(R.id.toolbar);
-        mToolbar.setTitle(R.string.app_name);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
 
+        this.login();
+    }
+
+    /**
+     * Logs into VK or shows friends if already logged in
+     */
+    private void login() {
         if (!VKSdk.isLoggedIn()) {
             VKSdk.login(this, "friends,offline");
         }
@@ -58,7 +56,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onError(VKError error) {
                 // Произошла ошибка авторизации (например, пользователь запретил авторизацию)
-                Toast.makeText(AppLoader.getAppContext(), "Authorization error", Toast.LENGTH_LONG).show();
+                showLoginError();
             }
         })) {
             super.onActivityResult(requestCode, resultCode, data);
@@ -69,16 +67,36 @@ public class MainActivity extends AppCompatActivity {
      * Shows friends list
      */
     private void showFriends() {
-        FriendsFragment fragment = new FriendsFragment();
+        showFragment(new FriendsFragment(), FriendsFragment.TAG);
+    }
 
+    /**
+     * Shows login error fragment
+     */
+    private void showLoginError() {
+        showFragment(new LoginErrorFragment(), LoginErrorFragment.TAG);
+        Toast.makeText(AppLoader.getAppContext(), R.string.auth_error, Toast.LENGTH_LONG).show();
+    }
+
+    /**
+     * Shows a fragment
+     * @param fragment Fragment instance
+     * @param tag String tag
+     */
+    private void showFragment(Fragment fragment, String tag) {
         FragmentManager fragmentManager = getFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.fragments_container, fragment, FriendsFragment.TAG);
+        fragmentTransaction.replace(R.id.fragments_container, fragment, tag);
         fragmentTransaction.commit();
     }
 
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public void onLoginAttempt() {
+        this.login();
     }
 }
