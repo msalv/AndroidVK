58995b4ff19af4e2d4756a2aab4b0430adc37c4b
Kirill Bykov <bikovgta13@gmail.com>
2016-01-27 23:15:53 +0300

    Fetch user's gender

--- app/src/main/java/org/kirillius/friendslist/fragments/FriendsFragment.java
+++ app/src/main/java/org/kirillius/friendslist/fragments/FriendsFragment.java
@@ -32,7 +32,7 @@ public class FriendsFragment extends Fragment {
 
     public static final String TAG = "FriendsFragment";
     private static final int FRIENDS_COUNT = 1000;
-    private static final String REQUEST_FIELDS = "online,last_seen,photo_50,photo_100,photo_200,photo_400";
+    private static final String REQUEST_FIELDS = "online,sex,last_seen,photo_50,photo_100,photo_200,photo_400";
 
     private LinearLayoutManager mLayoutManager;
     private FriendsAdapter mAdapter;
